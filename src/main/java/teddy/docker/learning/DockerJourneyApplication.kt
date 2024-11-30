67ad283f36eeac8e0d131845c1e8b14a48f701c9
thuandd <thuandd@apero.vn>
2024-11-30 21:19:10 +0700

    complete docker compose for api app

--- src/main/java/teddy/docker/learning/DockerJourneyApplication.kt
+++ src/main/java/teddy/docker/learning/DockerJourneyApplication.kt
@@ -1,12 +1,24 @@
 package teddy.docker.learning
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.boot.runApplication
+import org.springframework.context.ApplicationListener
+import org.springframework.context.annotation.Bean
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 //@EnableJpaRepositories
 @SpringBootApplication
-class NoteTakingApplication
+class NoteTakingApplication {
+    @Value("\${spring.datasource.url}")
+    lateinit var datasourceUrl: String
+
+    @Bean
+    fun printDatasourceUrl() = ApplicationListener<ApplicationReadyEvent> {
+        println("Datasource URL: $datasourceUrl")
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<NoteTakingApplication>(*args)
